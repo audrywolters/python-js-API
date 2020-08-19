bd9f445ff8899ca3b58ee9fe24c235bcf5663a9f
Audry Wolters <audrywolters@gmail.com>
2020-08-19 15:28:46 -0500

    POST works

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+# landing route
 @app.route('/')
 def index():
     return 'Oooh Hellooooooo'
 
+# a route with a name
 @app.route('/greet')
 def say_hello():
     return 'Server says Relax'
 
+# route accepts and prints a variable
 @app.route('/user/<username>')
 def show_user( username ):
     #return that user!
     return 'Username: %s' % username
-    
+
+#test a POST!
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        return 'POST and show a login page'
+    elif request.method == 'GET':
+        return 'GET and show a login page'
+
+
